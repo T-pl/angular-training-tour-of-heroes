8c17db1ee756cc0d7cfe2b21c278a283626bc706
T-pl <tiagofqu@gmail.com>
2024-03-03 11:16:03 -0300

    feature: add a new component

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,14 @@
+import { NgIf, UpperCasePipe } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { Hero } from '../hero';
+import { FormsModule } from '@angular/forms';
+@Component({
+  selector: 'app-hero-detail',
+  standalone: true,
+  imports: [FormsModule, UpperCasePipe, NgIf],
+  templateUrl: './hero-detail.component.html',
+  styleUrl: './hero-detail.component.css',
+})
+export class HeroDetailComponent {
+  @Input() hero?: Hero;
+}
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
-import { NgFor, UpperCasePipe } from '@angular/common';
+import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { HEROES } from '../mock-heroes';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [UpperCasePipe, FormsModule, NgFor],
+  imports: [UpperCasePipe, FormsModule, NgFor, NgIf, HeroDetailComponent],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css',
 })
 export class HeroesComponent {
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
   heroes = HEROES;
 }
